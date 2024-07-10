634ed33bef57bfee1535aed9e373eee71f87f0c0
andrewdev <a.miakshylo@gmail.com>
2024-07-09 18:13:26 -0600

    first commit + create a core app with user model

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
