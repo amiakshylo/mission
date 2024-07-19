afb859047dc407b3e70d54e393feefd64990e243
andrewdev <a.miakshylo@gmail.com>
2024-07-19 13:27:47 -0600

    creating admin view for category  and user_management

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from user_management import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name']
+
+
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
