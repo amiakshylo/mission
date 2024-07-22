708adaeb5167a4bffb2e203f87b1aff409e16138
andrewdev <a.miakshylo@gmail.com>
2024-07-22 10:09:43 -0600

    add UserCreate and User Serializers

---
+++ user_management/serializers.py
@@ -0,0 +1,17 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
+from .models import User
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = ('email', 'password')
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        model = User
+        fields = ['id', 'email', 'username']
+
+
