025baaf0d5b68566d5ac5b95deed0b12478b4a30
Allison Bliss <a.miakshylo@gmail.com>
2024-11-14 14:35:49 -0700

    changing urls prefixes

--- user_management/urls.py
+++ user_management/urls.py
@@ -4,12 +4,12 @@ from rest_framework_nested import routers
 from user_management import views
 
 router = routers.DefaultRouter()
-router.register("profile", views.UserProfileViewSet, basename="user_profile")
-router.register("goal", views.UserGoalViewSet, basename="user_goal")
-router.register("role", views.UserRoleViewSet, basename="role")
-router.register("area", views.UserAreaViewSet, basename="user_area")
-router.register("balance", views.UserBalanceViewSet, basename="user_balance")
-router.register("principle", views.UserPrincipleViewSet, basename='user_principle')
+router.register("profiles", views.UserProfileViewSet, basename="profile")
+router.register("goals", views.UserGoalViewSet, basename="goal")
+router.register("roles", views.UserRoleViewSet, basename="role")
+router.register("areas", views.UserAreaViewSet, basename="area")
+router.register("balances", views.UserBalanceViewSet, basename="balance")
+router.register("principles", views.UserPrincipleViewSet, basename='principle')
 
 urlpatterns = [
     path("", include(router.urls)),
