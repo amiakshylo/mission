cac742dd6f67140a3950978cdbd85c02109fd0b1
Allison Bliss <a.miakshylo@gmail.com>
2024-11-15 15:20:48 -0700

    deleting unusable files

--- goal_task/models.py
+++ goal_task/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 
 from core.models import TimeStampedModel, CompletedModel, PriorityModel
-from user_management.models import UserProfile, Role
 
 
 class Goal(TimeStampedModel):
@@ -13,14 +12,13 @@ class Goal(TimeStampedModel):
 
     GOAL_TYPE_CHOICES = ((TYPE_SHORT_TERM, "Short-term"), (TYPE_LONG_TERM, "Long-term"))
 
-    title = models.CharField(max_length=255, blank=False)
+    title = models.CharField(max_length=255, blank=False, unique=True)
     description = models.TextField()
-    is_custom = models.BooleanField(default=False, null=True)
-    impact_score = models.IntegerField(null=True, blank=True)
-    goal_type = models.CharField(choices=GOAL_TYPE_CHOICES, null=True, blank=True)
+    is_custom = models.BooleanField(default=False)
+    goal_type = models.CharField(choices=GOAL_TYPE_CHOICES, default='N/A')
     area = models.ManyToManyField("life_sphere.Area", related_name="goals")
     role = models.ManyToManyField("user_management.Role", related_name="goals")
-    hash = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    created_by = models.IntegerField(null=True)
 
     def __str__(self):
         return self.title
@@ -28,12 +26,12 @@ class Goal(TimeStampedModel):
 
 class GoalSuggestionLog(models.Model):
     user_profile = models.ForeignKey(
-        UserProfile, on_delete=models.CASCADE, related_name="suggestions"
+        'user_management.UserProfile', on_delete=models.CASCADE, related_name="suggestions"
     )
     goal = models.ForeignKey(
         "goal_task.Goal", on_delete=models.CASCADE, null=True, blank=True
     )
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True)
+    role = models.ForeignKey('user_management.Role', on_delete=models.CASCADE, null=True, blank=True)
     suggestion_source = models.CharField(max_length=255)  # 'openai' or 'ml_model'
     suggested_at = models.DateTimeField(auto_now_add=True)
     user_feedback = models.CharField(
